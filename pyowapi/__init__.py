e0978b468be4597a5b75356c04b509b0c3d10ee4
Tobias Messner <tobias@zusor.io>
2020-11-23 22:26:02 +0100

    Initial work on v2

--- pyowapi/__init__.py
+++ pyowapi/__init__.py
@@ -1,108 +1,121 @@
 import asyncio
 import aiohttp
-from typing import List, Union
+from typing import List, Union, Optional
 
 
 class Player:
-    def __init__(self, bnet: str, response: dict):
-        self.bnet: str = bnet
+    def __init__(self, player_name: str, response: dict, platform: str = "pc",  original_player_name: Optional[str] = None,):
+        self.player_name: str = player_name
+        self.original_player_name: Optional[str] = original_player_name
+        self.platform: str = platform
         self.success: bool = "error" not in response
         if self.success:
-            self.level: int = response["level"]
-            self.prestige: int = response["prestige"]
-            self.actual_level: int = self.prestige * 100 + self.level
-            self.private: bool = response["private"]
-            self.endorsement: int = response["endorsement"]
+            self.level: Optional[int] = response["level"]
+            self.prestige: Optional[int] = response["prestige"]
+            self.actual_level: Optional[int] = self.prestige * 100 + self.level
+            self.private: Optional[bool] = response["private"]
+            self.endorsement: Optional[int] = response["endorsement"]
 
             if not self.private:
-                self.quickplay_stats: dict = response["competitiveStats"]
-                self.quickplay_cards: int = self.quickplay_stats["awards"]["cards"]
-                self.quickplay_medals: int = self.quickplay_stats["awards"]["medals"]
-                self.quickplay_medals_bronze: int = self.quickplay_stats["awards"]["medalsBronze"]
-                self.quickplay_medals_silver: int = self.quickplay_stats["awards"]["medalsSilver"]
-                self.quickplay_medals_gold: int = self.quickplay_stats["awards"]["medalsGold"]
-                self.quickplay_games_won: int = self.quickplay_stats["games"]["won"]
+                self.quickplay_stats: Optional[dict] = response["competitiveStats"]
+                self.quickplay_cards: Optional[int] = self.quickplay_stats["awards"]["cards"]
+                self.quickplay_medals: Optional[int] = self.quickplay_stats["awards"]["medals"]
+                self.quickplay_medals_bronze: Optional[int] = self.quickplay_stats["awards"]["medalsBronze"]
+                self.quickplay_medals_silver: Optional[int] = self.quickplay_stats["awards"]["medalsSilver"]
+                self.quickplay_medals_gold: Optional[int] = self.quickplay_stats["awards"]["medalsGold"]
+                self.quickplay_games_won: Optional[int] = self.quickplay_stats["games"]["won"]
 
-                self.competitive_stats: dict = response["competitiveStats"]
-                self.competitive_cards: int = self.competitive_stats["awards"]["cards"]
-                self.competitive_medals: int = self.competitive_stats["awards"]["medals"]
-                self.competitive_medals_bronze: int = self.competitive_stats["awards"]["medalsBronze"]
-                self.competitive_medals_silver: int = self.competitive_stats["awards"]["medalsSilver"]
-                self.competitive_medals_gold: int = self.competitive_stats["awards"]["medalsGold"]
-                self.competitive_games_played: int = self.competitive_stats["games"]["played"]
-                self.competitive_games_won: int = self.competitive_stats["games"]["won"]
+                self.competitive_stats: Optional[dict] = response["competitiveStats"]
+                self.competitive_cards: Optional[int] = self.competitive_stats["awards"]["cards"]
+                self.competitive_medals: Optional[int] = self.competitive_stats["awards"]["medals"]
+                self.competitive_medals_bronze: Optional[int] = self.competitive_stats["awards"]["medalsBronze"]
+                self.competitive_medals_silver: Optional[int] = self.competitive_stats["awards"]["medalsSilver"]
+                self.competitive_medals_gold: Optional[int] = self.competitive_stats["awards"]["medalsGold"]
+                self.competitive_games_played: Optional[int] = self.competitive_stats["games"]["played"]
+                self.competitive_games_won: Optional[int] = self.competitive_stats["games"]["won"]
 
-                self.competitive_tank: Union[bool, int] = False
-                self.competitive_damage: Union[bool, int] = False
-                self.competitive_support: Union[bool, int] = False
+            self.competitive_tank: Union[bool, int] = False
+            self.competitive_damage: Union[bool, int] = False
+            self.competitive_support: Union[bool, int] = False
 
-                if response["ratings"]:
-                    for rating in response["ratings"]:
-                        if rating["role"] == "tank":
-                            self.competitive_tank = rating["level"]
-                        if rating["role"] == "damage":
-                            self.competitive_damage = rating["level"]
-                        if rating["role"] == "support":
-                            self.competitive_support = rating["level"]
+            if response.get("ratings"):
+                for rating in response["ratings"]:
+                    if rating["role"] == "tank":
+                        self.competitive_tank = rating["level"]
+                    if rating["role"] == "damage":
+                        self.competitive_damage = rating["level"]
+                    if rating["role"] == "support":
+                        self.competitive_support = rating["level"]
 
     def __repr__(self):
-        return f"<Player {self.bnet} success: {self.success}>"
+        return f"<Player {self.player_name} success: {self.success}>"
 
 
-async def _get_player_internal(player: str, session: aiohttp.ClientSession) -> Player:
+async def _correct_player_internal(session: aiohttp.ClientSession, incorrect_player_name: str, platform: str = "pc") -> Optional[Player]:
+    search_terms = []
+    if platform == "pc":
+        # Use full name to correct wrong capitalization
+        player_name_with_discriminator = incorrect_player_name.replace('#', '%23')
+        # Try short version to account for a wrong discriminator
+        player_name_short = incorrect_player_name.split('#')[0]
+        search_terms.append(player_name_with_discriminator)
+        search_terms.append(player_name_short)
+    else:
+        # Use full player name to correct wrong capitalization, there are no discriminators on console
+        search_terms.append(incorrect_player_name)
+
+    for search_term in search_terms:
+        async with session.get(f"https://playoverwatch.com/en-us/search/account-by-name/{search_term}") as r:
+            if r.status == 200:
+                profiles = await r.json()
+                # Filter the data to only include PC battletags
+                profiles = [profile for profile in profiles if profile["platform"] == platform]
+                # If we have one match that must be it
+                if len(profiles) == 1:
+                    new_player = await _get_player_internal(session, profiles[0]["name"], platform)
+                    if new_player.success:
+                        new_player.original_player_name = incorrect_player_name
+                        return new_player
+
+
+async def _get_player_internal(session: aiohttp.ClientSession, player_name: str, platform: str = "pc") -> Player:
     """
     Uses an aiohttp session to get a player. This is a coroutine and must be awaited.
-    :param player: String that is the players battletag
+    :param player_name: String that is the players name (battletag or other)
     :param session: An aiohttp ClientSession
     :return: A Player object
     """
     try:
-        async with session.get(f"https://ow-api.com/v2/stats/pc/{player.replace('#', '-')}/profile") as resp:
+        async with session.get(
+                f"https://ow-api.com/v2/stats/{platform}/{player_name.replace('#', '-')}/profile") as resp:
             data = await resp.json()
-            return Player(player, data)
+            return Player(player_name, data)
     except TimeoutError:
-        return Player(player, {"error": "timeout"})
+        return Player(player_name, {"error": "timeout"})
 
 
-async def _get_player(player: str) -> Player:
+async def _get_player(player_names: Union[str, List[str]], platform: str = "pc") -> Union[Player, List[Player]]:
     """
     This is a coroutine and must be awaited.
-    :param player: String that is the players battletag
+    :param player_names: String that is the players name (battletag or other)
     :return: A Player object
     """
     async with aiohttp.ClientSession() as session:
-        result = await _get_player_internal(player, session)
-        return result
+        if isinstance(player_names, list):
+            result = await asyncio.gather(*[_get_player_internal(session, player, platform) for player in player_names])
+            return result
+        else:
+            result = await _get_player_internal(session, player_names, platform)
+            return result
 
 
-def get_player(player: str) -> Player:
+def get_player(player_names: Union[str, List[str]], platform: str = "pc") -> Union[Player, List[Player]]:
     """
     Automatically creates event loop. Does not work with programs that already have an event loop, await _get_player instead
-    :param player: String that is the players battletag
+    :param player_names: String that is the players name (battletag or other)
+    :param platform: Platform, can be pc, xbox, ps4 and nintendo-switch
     :return: A Player object
     """
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(_get_player(player))
-    return result
-
-
-async def _get_bulk_players(players: List[str]) -> List[Player]:
-    """
-    This is a coroutine and must be awaited.
-    :param players: List of strings of the players battletags
-    :return: A list of Player objects
-    """
-    async with aiohttp.ClientSession() as session:
-        result = await asyncio.gather(*[_get_player_internal(player, session) for player in players])
-        return result
-
-
-def get_bulk_players(players: List[str]) -> List[Player]:
-    """
-    Automatically creates event loop. Does not work with programs that already have an event loop, await _get_bulk_players instead
-    :param players: List of strings of the players battletags
-    :return: A list of Player objects
-    """
-    loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(_get_bulk_players(players))
+    result = loop.run_until_complete(_get_player(player_names, platform))
     return result
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyowapi",
-    version="1.1.0",
+    version="2.0.0",
     author="Tobias Messner",
     author_email="tobias.d.messner@gmail.com",
     description="An asynchronous wrapper for ow-api.com using aiohttp",
